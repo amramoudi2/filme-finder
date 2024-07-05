aaf37ace82e013572bbbbc68835859279a11a416
amramoudi2 <amramoudi45@outlook.com>
2024-07-05 07:18:17 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
 let filmsDisplayPoz = document.getElementById("films");
 let noFilms = document.getElementById("no-film");
+let search = document.getElementById("search-input")
+let totalMovieList = []
 
-document.getElementById("search-btn").addEventListener("click", function(){
-    let s = document.getElementById("search-input").value
-    getMove(s)
+
+document.getElementById("btn-serch").addEventListener("click", function(){
+    getSearchValue()
 })
 
-document.addEventListener("click", function(e){
-    if(e.target.dataset.add){
-        let movieArr = []
-        let newMovieONClick = e.target.dataset.add
-
-        movieArr.push(newMovieONClick)
-
-        console.log(movieArr)
+search.addEventListener("keydown",function(e){
+    if (e.key === 'Enter'){
+        getSearchValue()
     }
 })
 
+
+document.addEventListener("click", function(e){
+    if(e.target.dataset.add){
+
+        let movieId = e.target.dataset.add;
+        let movieList = []
+        movieList.push(movieId)
+        totalMovieList.push(movieList[0])
+        localStorage.setItem(`${movieId}`, JSON.stringify(movieId))
+
+    }
+})
+
+function getSearchValue(){
+    let searchValue = search.value
+    getMove(searchValue)
+}
 
 function getMove(input){
     fetch(`http://www.omdbapi.com/?s=${input}&apikey=74892d35`)
         .then(res => res.json())
         .then(data => {
+            console.log(data)
             filmsDisplayPoz.innerText = ""
             displayMoves(data.Search)
+
+        }).catch(() => {
+            document.getElementById("no-movie-found").style.display = "block"
         })
 }
+
 
 function displayMoves(movesData){
     noFilms.style.display = "none";
@@ -40,7 +59,7 @@ function displayMoves(movesData){
     addMovieList.forEach((item) => {
         filmsDisplayPoz.innerHTML += `
              <div class="moves">
-                <img src=${item.Poster}>
+                <img alt="movie poster" src=${item.Poster}>
                 <div>
                     <h2>${item.Title}</h2>
                     <p>${item.Type}</p>
--- watch.js
+++ watch.js
@@ -1,3 +1,71 @@
-fetch("addedMovie.json")
-    .then(response => response.json())
-    .then(data => console.log(data))
+let moviesArr = []
+let noMoviesDisplay = document.getElementById("main-display")
+let displayMovise = document.getElementById("films")
+
+function getMovieId(movie) {
+    for ( let i = 0; i < localStorage.length; ++i ) {
+        movie.push(localStorage.getItem(localStorage.key(i)).slice(1,-1))
+    }
+}
+
+
+document.addEventListener("click", function (e) {
+    if(e.target.dataset.remove){
+
+        let dataSetTarget = e.target.dataset.remove;
+
+        noMoviesDisplay.style.display = "block"
+
+        localStorage.removeItem(dataSetTarget)
+
+        moviesArr = []
+        getMovieId(moviesArr)
+
+        displayMovise.innerHTML = ''
+
+        getMovies(moviesArr)
+    }
+    if(moviesArr.length <= 0){
+        noMoviesDisplay.style.display = "block"
+    }
+})
+
+
+function getMovies(movie){
+
+    for(let currentMovie of movie){
+        fetch(`http://www.omdbapi.com/?i=${currentMovie}&apikey=74892d35&`)
+            .then(res => res.json())
+            .then(data => {
+                if (moviesArr.length > 0){
+
+
+                    noMoviesDisplay.style.display = "none"
+                    displayMovise.style.display = "block"
+
+                    displayMovise.innerHTML += `
+                    <div class="moves">
+                        <img alt="movie poster" src=${data.Poster}>
+                        <div>
+                       <h2>${data.Title}</h2>
+                       <p>${data.Type}</p>
+                       <p>${data.Year}</p>
+                         <div class="add">
+                           <button data-remove = "${data.imdbID}">-</button>
+                           <p>remove</p>
+                         </div>
+                     </div>
+                 </div>
+                `
+                }else {
+                    noMoviesDisplay.style.display = "block"
+                    displayMovise.style.display = "none"
+                }
+
+            })
+    }
+}
+
+getMovieId(moviesArr)
+
+getMovies(moviesArr)
